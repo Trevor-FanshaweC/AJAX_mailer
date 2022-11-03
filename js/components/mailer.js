b9d1cdb27842ea0e89d13995e478885f4243822f
Trevor - Fanshawe <tvanrys@fanshaweonline.ca>
2022-11-03 15:26:43 -0400

    logic works now

--- js/components/mailer.js
+++ js/components/mailer.js
@@ -4,19 +4,15 @@
 
 async function SendMail(targetForm) {
     // mail stuff goes here
-    let formData = new FormData(targetForm);
-
-    // TODO: examine the method and incrementally build out the fetch call. currently it assumes POST
+    let formData = new FormData(targetForm),
+        formFieldErrors = false;
 
     let result = await fetch(`./includes/${targetForm.getAttribute("action")}`, {
         method: targetForm.method,
-        // TODO: a GET request doesn't use body, so this member should be conditional
         body: formData,
     }).then(response => {
         if (response.status !== 200) {
-            throw new Error(`Mail submission failed: ${response.status}`);
-
-            // could examine status code here and return it as part of an error object to the .catch handler in the SendMail then-able chain in main.js
+            formFieldErrors = true;
         }
 
         return response;
@@ -24,6 +20,10 @@ async function SendMail(targetForm) {
 
     let mailStatus = await result.json();
 
+    if (formFieldErrors) {
+        throw new Error(JSON.stringify(mailStatus));
+    }    
+    
     return mailStatus;
 }
 
--- js/main.js
+++ js/main.js
@@ -13,30 +13,22 @@ import { SendMail } from "./components/mailer.js";
         methods: {
             processMailFailure(result) {
                 // show a failure message in the UI
-                console.table(this.$refs); // table shows us an object in table form
-                alert('failure! and if you keep using an alert, DOUBLE failure!');
-
-                debugger;
-        
-                // show some UI here to let the user know the mail attempt was successful
+                // use this.$refs to connect to the elements on the page and mark any empty fields/inputs with an error class
+                alert('failure! and if you keep using an alert, DOUBLE failure!');        
+                // show some errors in the UI here to let the user know the mail attempt was successful
             },
 
             processMailSuccess(result) {
                 // show a success message in the UI
-                console.table(this.$refs); // table shows us an object in table form
                 alert("success! but don't EVER use alerts. They are gross.");        
                 // show some UI here to let the user know the mail attempt was successful
             },
 
-            processMail(event) {
-                // block the default submit behaviour
-                debugger;        
-                // use the SendMail component to try to process mail
+            processMail(event) {        
+                // use the SendMail component to process mail
                 SendMail(this.$el.parentNode)
                     .then(data => this.processMailSuccess(data))
                     .catch(err => this.processMailFailure(err));
-        
-                    // the error handler in the catch block could actually be a generic catch-and-display function that handles EVERY error you might encounter during runtime. Might be a better strategy to pass in a flag or just a message and have the function display it in the UI
             }
         }
     }).mount('#mail-form')
